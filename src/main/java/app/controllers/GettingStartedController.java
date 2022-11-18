ff1dd79d16f3b0e1ba11a15211a80a7b300aff0a
Max <66424051+MaxandreN@users.noreply.github.com>
2022-11-18 10:06:05 +0100

    web site add

--- src/main/java/app/controllers/GettingStartedController.java
+++ src/main/java/app/controllers/GettingStartedController.java
@@ -1,9 +1,10 @@
 package app.controllers;
 
+import lajavel.Controller;
 import lajavel.Response;
 import lajavel.View;
 
-public class GettingStartedController {
+public class GettingStartedController  extends Controller {
     public void index(Response response) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException {
         response.html(View.make("getting_started"));
     }
